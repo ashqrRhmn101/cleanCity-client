c7e63f9d19695142c500a56d6d1fb8279e07640b
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-11-09 13:10:19 +0600

    add Firebase configuration and setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDb5vysXB8sWjlWI-DTjEP6PoPo9GgUZHI",
+  authDomain: "clean-city-eb755.firebaseapp.com",
+  projectId: "clean-city-eb755",
+  storageBucket: "clean-city-eb755.firebasestorage.app",
+  messagingSenderId: "637630052515",
+  appId: "1:637630052515:web:99cc199ec8fed72fa86716",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
